1b4035353adabe7f3b0824be1441dd770ba0b508
Yury Levin <ylevin@corp.finam.ru>
2013-02-04 18:17:22 +0400

    create Stub class.

---
+++ library/src/main/java/ru/finam/bustard/BustardImpl.java
@@ -0,0 +1,18 @@
+package ru.finam.bustard;
+
+import com.google.common.collect.Multimap;
+
+/**
+ * Stub
+ */
+public class BustardImpl extends AbstractBustard {
+    @Override
+    void initialize(Multimap<Class<?>, Class<?>> eventTypes) {
+        // Do nothing
+    }
+
+    @Override
+    void post(Object listener, Object event) throws Throwable {
+        throw new UnsupportedOperationException("Stub bustard using.");
+    }
+}
