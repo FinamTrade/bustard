2e4c63a222725315abeae25577a93ba09bbfb730
Boris Aksenov <miofthena@gmail.com>
2013-03-16 02:05:19 +0400

    add Topic annotation

---
+++ bustard-core/src/main/java/ru/finam/bustard/Topic.java
@@ -0,0 +1,19 @@
+package ru.finam.bustard;
+
+import javax.inject.Qualifier;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Bustard channel topic qualifier
+ * User: boris
+ * Date: 3/16/13
+ */
+@Qualifier
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Topic {
+    String value();
+}
