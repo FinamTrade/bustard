c87c69bd2d144a7bc3264a8b69bbb567f5de29b2
nordligulv <a.korzhevskiy@gmail.com>
2013-04-10 15:54:43 +0400

    fix loading channels

--- bustard-dagger/src/main/java/ru/finam/bustard/codegen/ChannelsFinder.java
+++ bustard-dagger/src/main/java/ru/finam/bustard/codegen/ChannelsFinder.java
@@ -7,7 +7,8 @@ import java.util.List;
 
 public class ChannelsFinder implements ChannelsConsts {
 
-    public static final String FILE_PATH = CHANNELS_PACKAGE_NAME.replace('.', '/') + CHANNELS_FILE_NAME;
+    public static final String FILE_PATH = CHANNELS_PACKAGE_NAME.replace('.', '/') +
+        "/" + CHANNELS_FILE_NAME;
 
     public static List<String> retrieveChannelKeys() throws IOException {
         return FileLinesParser.retrieveResource(ChannelModule.class.getClassLoader(), FILE_PATH);
