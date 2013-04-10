3049a9a469c09fa8a12db62ba2f903e78acb9062
nordligulv <a.korzhevskiy@gmail.com>
2013-04-10 14:44:49 +0400

    fix

--- bustard-core/src/main/java/ru/finam/bustard/codegen/FileLinesParser.java
+++ bustard-core/src/main/java/ru/finam/bustard/codegen/FileLinesParser.java
@@ -4,6 +4,7 @@ import com.google.common.base.Charsets;
 import com.google.common.collect.Iterators;
 import com.google.common.io.Resources;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URL;
 import java.util.*;
@@ -11,11 +12,15 @@ import java.util.*;
 public class FileLinesParser {
 
     public static List<String> retrieveResource(ClassLoader loader, String filePath) throws IOException {
-        Iterator<URL> urls = Iterators.forEnumeration(loader.getResources(filePath));
         List<String> result = new ArrayList<String>();
-        while (urls.hasNext()) {
-            URL url = urls.next();
-            result.addAll(Resources.readLines(url, Charsets.UTF_8));
+        try {
+            Iterator<URL> urls = Iterators.forEnumeration(loader.getResources(filePath));
+            while (urls.hasNext()) {
+                URL url = urls.next();
+                result.addAll(Resources.readLines(url, Charsets.UTF_8));
+            }
+        } catch (FileNotFoundException e) {
+            //ignore exception silently if and return empty list
         }
         return result;
     }
