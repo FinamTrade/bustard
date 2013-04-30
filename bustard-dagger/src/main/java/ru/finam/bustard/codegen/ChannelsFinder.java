c73a39a97fccceb9e8cd68fb8ed7065177a47e22
Boris Aksenov <aksenov@corp.finam.ru>
2013-04-30 16:51:43 +0400

    fix class loader

--- bustard-dagger/src/main/java/ru/finam/bustard/codegen/ChannelsFinder.java
+++ bustard-dagger/src/main/java/ru/finam/bustard/codegen/ChannelsFinder.java
@@ -1,14 +1,16 @@
 package ru.finam.bustard.codegen;
 
+import ru.finam.bustard.Bustard;
+
 import java.io.IOException;
 import java.util.List;
 
 public class ChannelsFinder implements ChannelsConsts {
 
     public static final String FILE_PATH = BUSTARD_PACKAGE_NAME.replace('.', '/') +
-        "/" + CHANNELS_FILE_NAME;
+            "/" + CHANNELS_FILE_NAME;
 
     public static List<String> retrieveChannelKeys() throws IOException {
-        return FileLinesParser.retrieveResource(Thread.currentThread().getContextClassLoader(), FILE_PATH);
+        return FileLinesParser.retrieveResource(Bustard.class.getClassLoader(), FILE_PATH);
     }
 }
