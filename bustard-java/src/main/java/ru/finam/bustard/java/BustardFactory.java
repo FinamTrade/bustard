933532a3a597d6c11cec42c1a12ac70cda2556a2
Yury Levin <ylevin@corp.finam.ru>
2013-04-30 14:47:53 +0400

    Fix package for BustardImpl.

--- bustard-java/src/main/java/ru/finam/bustard/java/BustardFactory.java
+++ bustard-java/src/main/java/ru/finam/bustard/java/BustardFactory.java
@@ -6,7 +6,7 @@ import ru.finam.bustard.codegen.Consts;
 public class BustardFactory implements Consts {
     public static Bustard createBustard() {
         try {
-            Class bustardType = Class.forName(BUSTARD_PACKAGE_NAME + "." + BUSTARD_IMPL_NAME);
+            Class bustardType = Class.forName(BUSTARD_JAVA_PACKAGE_NAME + "." + BUSTARD_IMPL_NAME);
             return (Bustard) bustardType.newInstance();
         } catch (Exception e) {
             throw new RuntimeException(e);
